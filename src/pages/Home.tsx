791edccfac2a0d58384bf0c6261701d1e7c37dbd
Felipe del Pozo <felipedelpozo@dtoo.es>
2022-01-05 06:37:05 +0100

    Update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,12 @@ const markdown = `
   This project has been developed using the following technologies:
 
   * [React](https://reactjs.org/)
-  * [React Hooks](https://reactjs.org/docs/hooks-intro.html)
+  * [TypesScript](https://www.typescriptlang.org/)
   * [Firebase](https://firebase.google.com/)
+  * [React Hooks](https://reactjs.org/docs/hooks-intro.html)
+  * [Material UI](https://material-ui.com/)
   * [MathJS](https://mathjs.org/)
   * [React Query Firestore](https://github.com/aminerol/react-query-firestore)
-  * [Material UI](https://material-ui.com/)
   * [React Infinite Scroll Component](https://www.npmjs.com/package/react-infinite-scroll-component)
   * [React Markdown](https://github.com/remarkjs/react-markdown)
   * [React Router](https://reacttraining.com/react-router/web/guides/quick-start)
